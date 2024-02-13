00c27cfc79864b2fb40c05a8aba61ee522a529f7
Vicente Sebastian Álvarez Olvera <190300376@ucaribe.edu.mx>
2024-02-13 16:55:24 -0500

    Added comments

--- aldatum.java
+++ aldatum.java
@@ -5,20 +5,29 @@ import java.io.FileReader;
 import java.util.Iterator; 
 import java.util.Map; 
 
+//Imports for using JSONs
 import org.json.simple.JSONArray; 
 import org.json.simple.JSONObject; 
 import org.json.simple.parser.*;
 
+
 public class aldatum {
     @SuppressWarnings("deprecation")
+    //Main function, which will connect to the web service using the link and the authentication code
     public static void main(String[] args) throws Exception{
         String Link = "https://elastic.snaplogic.com/api/1/rest/slsched/feed/Partners/AllDatum/Entrevista_Integracion/LeeArchivoNombresTask";
         URL enlace = new URL(Link);
         String authen = "h8JLQvfj5Yl1iQeOvBT43d17RoDBO6UQ";
+
+        //Creates a connection 
         HttpURLConnection connection = (HttpURLConnection) enlace.openConnection();
+        //Specifies the type of request and its properties
         connection.setRequestMethod("GET");
         connection.setRequestProperty("Authorization", "Bearer " + authen);
+
+        //Creates a buffered readder in order to recieve the JSON from the request
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
         //    StringBuilder response = new StringBuilder();
         //    String inputLine;
         //    while ((inputLine = in.readLine()) != null) {
