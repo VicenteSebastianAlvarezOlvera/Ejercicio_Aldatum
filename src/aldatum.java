6c1d322b017e0a6b2fccef27ab777f039936f413
Vicente Sebastian Álvarez Olvera <190300376@ucaribe.edu.mx>
2024-02-15 23:55:42 -0500

    Commented code in Spanish, downloaded payload, changed parameter to live

--- src/aldatum.java
+++ src/aldatum.java
@@ -3,116 +3,130 @@ import java.io.*;
 import java.util.Comparator;
 
 
-//Imports for using JSONs
-import org.json.simple.JSONArray; 
-import org.json.simple.JSONObject; 
+//Importaciones para usar JSONs
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 
+
+
 public class aldatum {
-    @SuppressWarnings({ "deprecation", "unchecked" })
-    //Main function, which will connect to the web service using the link and the authentication code
+    //Se crea la función principal, la cual realizará todo el procedimiento
     public static void main(String[] args) throws Exception{
-        //Link of the Source Web Service
+        //Enlace del Source Web Service
         String SourceWebService = "https://elastic.snaplogic.com/api/1/rest/slsched/feed/Partners/AllDatum/Entrevista_Integracion/LeeArchivoNombresTask";
-        
-        //Link of the parameters
+       
+        //Los parámetros a usar con el Source Web Service
         String archivoSWS = "first_names";
         String extensionSWS = "txt";
-        //We create the authentication token
+        //Se almacena el token de autenticación
         String authenSWS = "Bearer h8JLQvfj5Yl1iQeOvBT43d17RoDBO6UQ";
 
-        //We create the final link, combining the Source Web Service and its 2 parameters, and create the URL to be used
+
+        //Se crea el link final, combinando el link del Source Web Service con los dos parámetros a usar, y creamos una variable tipo URL
         String LinkSWS = SourceWebService + "?archivo=" + archivoSWS + "&extension=" + extensionSWS;
         URL enlaceSWS = new URL(LinkSWS);
-        
-        //Creates a connection 
+       
+        //Se crea la conexión al enlace
         HttpURLConnection connection = (HttpURLConnection) enlaceSWS.openConnection();
-        //Specifies the type of request and its authentication key
+        //Se especifica el tipo de solicitud, en este caso GET, y pasamos el token de autenticación
         connection.setRequestMethod("GET");
         connection.setRequestProperty("Authorization", authenSWS);
 
-        ////Creates a buffered readder in order to get the response
+
+        //Se crea un bufferedReader para leer los datos de la conexión
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-        StringBuilder response = new StringBuilder();
+
+        //Se guarda la respuesta en inputLine
+        StringBuilder respuesta = new StringBuilder();
         String inputLine;
         while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-            //System.out.println(inputLine);
+            respuesta.append(inputLine);
         }
         in.close();
 
-        //Creates a JSON parser to get the response 
+
+        //Se crea un parser JSON para guardar la respuesta
         JSONParser parser=new JSONParser();
-        Object object = parser.parse(response.toString());
-        
-        //Converts the response to JSON format
+        Object object = parser.parse(respuesta.toString());
+       
+        //Se convierte la respuesta a JSON
         JSONArray array = (JSONArray) object;        
 
-        //Sort by name
-        try {
-            // Sort the JSONArray by the "NAME" field
-            array.sort(Comparator.comparing(obj -> ((JSONObject) obj).get("NAME").toString()));
 
-            // Print the sorted JSONArray
-            //System.out.println(array.toJSONString());
+        //Se ordena el contenido de JSON de manera alfabética
+        try {
+            array.sort(Comparator.comparing(obj -> ((JSONObject) obj).get("NAME").toString()));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //Gets the sum of all names
-        int totalSum = 0;
+       
+        //Se suman los valores de los nombres usando la función CalcularValorDelNombre
+        int totalSuma = 0;
         for (int i = 0; i < array.size(); i++) {
             JSONObject obj = (JSONObject) array.get(i);
             String name = (String) obj.get("NAME");
-            int nameScore = calculateNameScore(name) * (i + 1); // Multiply by alphabetical position
-            //obj.put("value", nameScore);
-            //obj.put("position", i + 1);
-            totalSum += nameScore;
+            //Se multiplica el valor de cada nombre por su posición alfabética
+            int nameScore = CalcularValorDelNombre(name) * (i + 1);
+            obj.put("value", nameScore);
+            obj.put("position", i + 1);
+            totalSuma += nameScore;
         }
 
-        //Creates JSON to send
-        JSONObject resultado = new JSONObject();
-        resultado.put("ResultadoObtenido", totalSum);
-        System.out.println(resultado);
 
-        //Prepare link, parameters and authentication to send delivery
+        //Se crea un JSON para enviar el resultado
+        JSONObject resultado = new JSONObject();
+        resultado.put("ResultadoObtenido", totalSuma);
+
+
+        //Se prepara el link, los parámetros, y la autenticación
         String TargetWebService = "https://elastic.snaplogic.com/api/1/rest/slsched/feed/Partners/AllDatum/Entrevista_Integracion/VerificaResultadoEjercicioTecnico1ALLDATUMTask";
-        //Link of the parameters
+        //Contenido de los parámetros
         String archivoTWS = "first_names";
         String extensionTWS = "txt";
         String nombreTWS = "Vicente_Alvarez";
-        String pruebaTWS = "1";
-        //We create the authentication token
+        String pruebaTWS = "0";
+        //Token de autenticación
         String authenTWS = "Bearer giqJWNuzhOnDTYaa1Diy1jw7FQhqZSwl";
 
-        //We create the final link, combining the Source Web Service and its 2 parameters, and create the URL to be used
+
+        //Se crea el enlace final, concatenando el TargetWebService y sus 4 parámetros
         String LinkTWS = TargetWebService + "?archivo=" + archivoTWS + "&extension=" + extensionTWS + "&nombre=" + nombreTWS + "&prueba=" + pruebaTWS;
-        //System.out.println(LinkTWS);
         URL enlaceTWS = new URL(LinkTWS);
 
-        //Creates a connection 
+
+        //Se inicia la conexión
         HttpURLConnection connectionTWS = (HttpURLConnection) enlaceTWS.openConnection();
-        ////Specifies the type of request and its authentication key
+        //Se especifica el tpo de respuesta, el tipo de contenido a enviar, el token de autorización, y se configura para poder mandar datos
         connectionTWS.setRequestMethod("POST");
         connectionTWS.setRequestProperty("Authorization", authenTWS);
         connectionTWS.setRequestProperty("Content-Type", "application/json");
         connectionTWS.setDoOutput(true);
 
+
+        //Se envía el contenido del JSON
         try(OutputStream os = connectionTWS.getOutputStream()) {
             byte[] input = resultado.toString().getBytes("utf-8");
             os.write(input, 0, input.length);
         }
+
+
+        //Se obtiene el estado de la respuesta
         int responseCode = connectionTWS.getResponseCode();
         System.out.println("Response Code: " + responseCode);
 
+
     }
 
-    private static int calculateNameScore(String name) {
-        int score = 0;
-        for (char c : name.toCharArray()) {
-            score += Character.toUpperCase(c) - 'A' + 1; // Calculate alphabetical value
+
+    //Función para calcular el valor de cada nombre
+    private static int CalcularValorDelNombre(String nombre) {
+        int valor = 0;
+        for (char c : nombre.toCharArray()) {
+            valor += Character.toUpperCase(c) - 'A' + 1; // Calcula el valor alfabético
         }
-        return score;
+        return valor;
     }
 }
